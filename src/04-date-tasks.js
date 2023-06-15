e07f4a61766714484d58ae522aeffad5933d691f
LqosL <camilla_agliullina@mail.ru>
2023-06-15 12:43:06 +0600

    feat: solve date section

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -12,15 +12,15 @@
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example:
  *    'December 17, 1995 03:24:00'    => Date()
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -28,14 +28,14 @@ function parseDataFromRfc2822(/* value */) {
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example :
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -43,8 +43,8 @@ function parseDataFromIso8601(/* value */) {
  * Returns true if specified date is leap year and false otherwise
  * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
- * @param {date} date
- * @return {bool}
+ * @param {Date} date
+ * @return {boolean}
  *
  * @example :
  *    Date(1900,1,1)    => false
@@ -53,8 +53,18 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date.getFullYear());
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100 === 0) {
+    return false;
+  }
+  if (year % 4 === 0) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -62,8 +72,8 @@ function isLeapYear(/* date */) {
  * Returns the string representation of the timespan between two dates.
  * The format of output string is "HH:mm:ss.sss"
  *
- * @param {date} startDate
- * @param {date} endDate
+ * @param {Date} startDate
+ * @param {Date} endDate
  * @return {string}
  *
  * @example:
@@ -73,8 +83,24 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let hh = '00';
+  let mm = '00';
+  let ss = '00';
+  let sss = '000';
+  if (endDate.getHours() - startDate.getHours() > 0) {
+    hh = (endDate.getHours() - startDate.getHours()).toString().padStart(2, '0');
+  }
+  if (endDate.getMinutes() - startDate.getMinutes() > 0) {
+    mm = (endDate.getMinutes() - startDate.getMinutes()).toString().padStart(2, '0');
+  }
+  if (endDate.getSeconds() - startDate.getSeconds() > 0) {
+    ss = (endDate.getSeconds() - startDate.getSeconds()).toString().padStart(2, '0');
+  }
+  if (endDate.getMilliseconds() - startDate.getMilliseconds() > 0) {
+    sss = (endDate.getMilliseconds() - startDate.getMilliseconds()).toString().padStart(3, '0');
+  }
+  return `${hh}:${mm}:${ss}.${sss}`;
 }
 
 
@@ -94,8 +120,20 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() % 12;
+  const minutes = date.getUTCMinutes();
+
+  const hoursPos = hours * 30 + (minutes / 12) * 6;
+  const minutesPos = minutes * 6;
+
+  let angle;
+  if (Math.abs(hoursPos - minutesPos) < 180) {
+    angle = Math.abs(hoursPos - minutesPos);
+  } else {
+    angle = 360 - Math.abs(hoursPos - minutesPos);
+  }
+  return (angle * Math.PI) / 180;
 }
 
 
